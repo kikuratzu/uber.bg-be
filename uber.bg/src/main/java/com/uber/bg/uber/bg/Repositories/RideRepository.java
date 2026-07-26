4a934feee4a6b5428a56a9088c5a546b6d2d0182
kikuratzu <kirilkrushev@gmail.com>
2026-07-26 17:04:30 +0300

    m

--- uber.bg/src/main/java/com/uber/bg/uber/bg/Repositories/RideRepository.java
+++ uber.bg/src/main/java/com/uber/bg/uber/bg/Repositories/RideRepository.java
@@ -1,4 +1,11 @@
 package com.uber.bg.uber.bg.Repositories;
 
-public interface RideRepository {
+import com.uber.bg.uber.bg.Entities.Ride;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RideRepository extends JpaRepository<Ride, UUID> {
 }
